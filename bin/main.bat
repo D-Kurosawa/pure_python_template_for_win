f267ccb0c361515e32d96ed86c84003573792a16
Daisuke Kurosawa <dai-kuro@sings.jp>
2020-02-06 13:48:44 +0900

    [update] bin script

--- bin/main.bat
+++ bin/main.bat
@@ -1,6 +1,9 @@
 @echo off
+set env_name=py38_env
 set project_name=project_name
 set execpath=%cd%
+
+call conda.bat activate %env_name%
 
 :: Move script path to current
 cd /d %~dp0
@@ -8,6 +11,8 @@ cd ../
 
 @echo on
 python -m %project_name% conf/config.json
+
+call conda.bat deactivate
 
 @echo off
 cd /d %execpath%
