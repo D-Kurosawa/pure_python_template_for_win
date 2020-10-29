d8b9b94786203909dc4947aef2829138aad64171
Daisuke Kurosawa <dai-kuro@sings.jp>
2020-10-29 16:34:00 +0900

    [update] create_requirements_text.bat

--- requirements/create_requirements_text.bat
+++ requirements/create_requirements_text.bat
@@ -1,12 +1,10 @@
 @echo off
-:: 仮想環境名の入力要求
 set /p env_name="Input virtual enviroment name : "
 echo on
 
 call conda.bat activate %env_name%
 
 @echo off
-:: activate失敗時に"errorlevel"に1が入ることを利用してエラーチェック
 if %errorlevel% neq 0 (
   echo;
   echo Error : Can not activate [%env_name%]
